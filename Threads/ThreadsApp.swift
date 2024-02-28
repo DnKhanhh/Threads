60c7727114980e20cff31601b52eb8c8dd39b44a
Brian <dnkhanhdev@gmail.com>
2024-02-28 16:05:50 +0700

    Initial Commit

---
+++ Threads/ThreadsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsApp.swift
+//  Threads
+//
+//  Created by Brian on 28/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
